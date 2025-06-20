b80676e7b78cf9f7149c78e5a746bd6b06540c50
JoyJudy Wangui <judyjwangui@gmail.com>
2025-06-20 10:22:25 +0300

    Implementation of bubble sort and selection sort

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,28 @@
-def bubble_sort(unsorted_list):
-    # TODO: Implement bubble sort
-    pass
+def bubble_sort(array):
+    #get length of the array
+    array_length = len(array)
+
+    #runs once for each element
+    for i in range(array_length):
+        swapped = False
+
+        #compare adjacent elements
+        #avoids the last i elements (already sorted)
+        for j in range(array_length - i - 1):
+            #if element on the left > element on the right, swap
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
+                swapped = True
+
+            #if no swaps happened in this pass, the list is already sorted
+        if not swapped:
+            break
+    return array
+
+my_list = [1, 3, 5, 7, 2, 4, 6, 8]
+print(f"Original list: {my_list}")
+bubble_sort(my_list)
+print(f"Sorted list: {my_list}")
+
+     # TODO: Implement bubble sort
+    # pass
--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,25 @@
-def selection_sort(arr):
+def selection_sort(array):
+    array_length = len(array)
+
+    for i in range(array_length):
+        #i contains the smallest index
+        min_index = i
+
+        for j in range(i + 1, array_length):
+            #find an element smaller than the current minimum
+            if array[j] < array[min_index]:
+                #update the index of the minimum element
+                min_index = j
+
+        if min_index != i:
+            #swap  the minimum element with the element in position i
+            array[i], array[min_index] = array[min_index], array[i]
+
+    return array
+
+my_list = [1,34,6,78,2,99]
+print(f"Original list: {my_list}")
+selection_sort(my_list)
+print(f"Sorted list: {my_list}")
     # TODO: Implement selection sort
-    pass
+    # pass
